63c74d2d6fb1b2d086221f59e81c773dbd97bdf2
rzmilon <rzmilon@gmail.com>
2022-12-18 01:03:11 +0600

    update login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import loginImg from '../../assets/images/login/login.svg';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const Login = () => {
+    const {login} = useContext(AuthContext);
 
-       const handelLogin = event=>{
-            event.preventDefault();
-        }
+    const handelLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        login(email, password)
+        .then(result =>{
+            const user = result.user;
+            console.log(user);
+        } )
+        .catch(err => console.error(err))
+    }
 
 
     return (
@@ -16,7 +28,7 @@ const Login = () => {
                     <img className='w3/4' src={loginImg} alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
-                <h1 className="text-5xl text-center font-bold">Login</h1>
+                    <h1 className="text-5xl text-center font-bold">Login</h1>
                     <form onSubmit={handelLogin} className="card-body">
                         <div className="form-control">
                             <label className="label">
@@ -30,7 +42,7 @@ const Login = () => {
                             </label>
                             <input type="text" name='password' placeholder="password" className="input input-bordered" />
                             <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                <Link to="#" className="label-text-alt link link-hover">Forgot password?</Link>
                             </label>
                         </div>
                         <div className="form-control mt-6">
--- src/Pages/Signup/SignUp.js
+++ src/Pages/Signup/SignUp.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import loginImg from '../../assets/images/login/login.svg';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const SignUp = () => {
+    const {createUser} = useContext(AuthContext);
+    
     const handelSignUp = event => {
         event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+        .then(result =>{
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(err => console.error(err))
     }
 
 
